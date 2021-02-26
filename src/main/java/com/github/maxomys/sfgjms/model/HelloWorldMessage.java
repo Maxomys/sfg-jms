da085fde2eca4239a26e58f22b2c0f660b5b017b
Zaphod <mistrz7losos@live.com>
2021-02-26 01:13:17 +0100

    Commit :)

---
+++ src/main/java/com/github/maxomys/sfgjms/model/HelloWorldMessage.java
@@ -0,0 +1,22 @@
+package com.github.maxomys.sfgjms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = -3633278830217965623L;
+
+    private UUID id;
+    private String message;
+
+}
